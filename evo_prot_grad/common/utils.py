1cd3a2bdcd8318bf472dfbc0c7e95424344d4936
Dakota Hawkins <dhawkins@fogpharma.com>
2026-03-30 09:56:20 -0400

    added staple token to canonical alphabet. ruff styling.

--- evo_prot_grad/common/utils.py
+++ evo_prot_grad/common/utils.py
@@ -1,15 +1,38 @@
-import torch 
-import numpy as np
 import random
+from pathlib import Path
 from typing import List
-from pathlib import Path 
 
-CANONICAL_ALPHABET = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
+import numpy as np
+import torch
+
+CANONICAL_ALPHABET = [
+    "A",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "V",
+    "W",
+    "Y",
+    "$",
+]
 
 
 def safe_logits_to_probs(logits: torch.Tensor) -> torch.Tensor:
     """safe convert logits to probs.
-    
+
     Args:
         logits (torch.Tensor): [parallel_chains, seq_len, vocab_size]
 
@@ -24,21 +47,21 @@ def safe_logits_to_probs(logits: torch.Tensor) -> torch.Tensor:
 
 def mut_distance(x, wt):
     """Computes edit distance from x to wt
-    
+
     Args:
         x (torch.Tensor): shape [parallel_chains, seq_len, vocab_size].
         wt (torch.Tensor): shape [1, seq_len, vocab_size].
     Returns:
         edits (torch.Tensor): shape [parallel_chains].
     """
-    wt = wt.repeat(x.size(0),1,1)
+    wt = wt.repeat(x.size(0), 1, 1)
     edits = ((x != wt).float().sum(-1) > 0).float().sum(-1)
     return edits
 
 
 def mutation_mask(x, wt, mutations_value=False):
     """Create a boolean tensor with locations corresponding to mutations set to `mutations_value`.
-    
+
     For every pos where x and wt differ, and wt is not a gap (0), the mask is set to `mutations_value`.
     Everywhere else set to `~mutations_value`.
 
@@ -51,14 +74,16 @@ def mutation_mask(x, wt, mutations_value=False):
     """
     mask = torch.ones_like(x).to(x.device)
     if wt.shape[0] == 1:
-        wt = wt.repeat(x.size(0),1,1)
+        wt = wt.repeat(x.size(0), 1, 1)
     positions = (x != wt) & (wt == 1)
     mask[positions] = 0
     m = mask.bool()
     return ~m if mutations_value else m
 
 
-def expert_alphabet_to_canonical(expert_alphabet: List[str], device: str) -> torch.Tensor:
+def expert_alphabet_to_canonical(
+    expert_alphabet: List[str], device: str
+) -> torch.Tensor:
     """Create a binary matrix that shuffles the vocab dimension of a tensor
        in an expert's AA alphabet order to the canonical AA alphabet order.
 
@@ -67,8 +92,9 @@ def expert_alphabet_to_canonical(expert_alphabet: List[str], device: str) -> tor
     Returns:
         alignment_matrix (torch.Tensor): tensor of shape [len(expert_alphabet), len(CANONICAL_ALPHABET)].
     """
-    alignment = torch.zeros(len(expert_alphabet),
-                            len(CANONICAL_ALPHABET), device=device)
+    alignment = torch.zeros(
+        len(expert_alphabet), len(CANONICAL_ALPHABET), device=device
+    )
     for i, aa in enumerate(expert_alphabet):
         if aa in CANONICAL_ALPHABET:
             alignment[i, CANONICAL_ALPHABET.index(aa)] = 1
@@ -89,7 +115,7 @@ def set_seed(seed: int) -> None:
 
 def print_variant_in_color(seq: str, wt: str, ignore_gaps: bool = True) -> None:
     """Print a variant sequence with highlighted mutations.
-    
+
     Args:
         seq (str): The variant sequence.
         wt (str): The wildtype sequence.
@@ -97,20 +123,20 @@ def print_variant_in_color(seq: str, wt: str, ignore_gaps: bool = True) -> None:
     """
     for j in range(len(wt)):
         if seq[j] != wt[j]:
-            if ignore_gaps and (seq[j] == '-' or seq[j] == 'X'):
+            if ignore_gaps and (seq[j] == "-" or seq[j] == "X"):
                 continue
-            print(f'\033[91m{seq[j]}', end='')
+            print(f"\033[91m{seq[j]}", end="")
         else:
-            print(f'\033[0m{seq[j]}', end='')
-    print('\033[0m')
+            print(f"\033[0m{seq[j]}", end="")
+    print("\033[0m")
 
 
 def read_fasta(fasta_file: str) -> str:
     """Read a fasta, return string."""
-    with open(Path(fasta_file), 'r') as f:
+    with open(Path(fasta_file), "r") as f:
         for line in f:
-            if line[0] != '>':
+            if line[0] != ">":
                 seq = line.strip()
                 # Add a space between each amino acid
-                seq = ' '.join(seq)
-                return seq    
+                seq = " ".join(seq)
+                return seq
